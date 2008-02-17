b40812b62db27c0a7b2573ccaf36d429da1baa6e
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-02-17 10:32:09 +0000

    auto commit

--- webtornado/www/cgi-bin/action.pl
+++ webtornado/www/cgi-bin/action.pl
@@ -7,6 +7,7 @@ my $wt = new WT;
 my $u = $ENV{REMOTE_USER};
 sub cp { $wt->cgi->param(shift) }
 
+=cut
 ({
     'stop' => sub {
 	(cp('id') eq 'all') 
@@ -25,5 +26,34 @@ sub cp { $wt->cgi->param(shift) }
 	$wt->do('UPDATE torrents SET maxratio = ? WHERE id = ? AND owner = ?', undef, cp('maxratio'), cp('id'), $u);
     },
 }->{cp('action')} or sub {})->();
+=cut
+
+use DBIx::Perlish;
+DBIx::Perlish::init($wt->dbh);
+
+({
+    'stop' => sub {
+	db_update { 
+	    torrents->owner == $u; 
+	    cp('id') eq 'all' or torrents->id == cp('id');
+	    torrents->active = 0;
+	};
+#	(cp('id') eq 'all') 
+#	    ? $wt->do('UPDATE torrents SET active = 0 WHERE owner = ?', undef, $u)
+#	    : $wt->do('UPDATE torrents SET active = 0 WHERE id = ? AND owner = ?', undef, cp('id'), $u);
+    }, 
+    'start' => sub {
+    	(cp('id') eq 'all') 
+	    ? $wt->do('UPDATE torrents SET active = 1 WHERE owner = ?', undef, $u)
+	    : $wt->do('UPDATE torrents SET active = 1 WHERE id = ? AND owner = ?', undef, cp('id'), $u);	    
+    },
+    'delete' => sub {
+	$wt->do('UPDATE torrents SET active = 0, del = 1 WHERE id = ? AND owner = ?', undef, cp('id'), $u);
+    },
+    'set_maxratio' => sub {
+	$wt->do('UPDATE torrents SET maxratio = ? WHERE id = ? AND owner = ?', undef, cp('maxratio'), cp('id'), $u);
+    },
+}->{cp('action')} or sub {})->();
+
 
 print $wt->cgi->header(-location => '/', -status => 302);
