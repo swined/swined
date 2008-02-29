089be8622c7e18a782ac5939537400a81fee41d5
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-02-29 11:55:55 +0000

    auto commit

--- trunk/webtornado/www/cgi-bin/list.pl
+++ trunk/webtornado/www/cgi-bin/list.pl
@@ -61,9 +61,6 @@ while (my $r = $sth->fetchrow_hashref) {
     $statusimg = IMG('/img/yellow.gif') if $r->{active} and ! $r->{pid} or ! $r->{active} and $r->{pid};
     $statusimg .= A("/$r->{id}.tar", IMG('/img/tar_down.gif')) if $r->{done} and not $r->{del};
     $statusimg .= A("/delete/$r->{id}", IMG('/img/delete.png')) unless $r->{del};
-    my $files = join '<br>', map { '[' . fmsz($_->{size}) . '] ' . $_->{name} } @{$bt->{files}};
-    my $fc = scalar @{$bt->{files}};
-    $files = $fc > 1 ? "<div id='files_$r->{id}' style='color: #666666; display: inline'><a onclick='files_$r->{id}.innerHTML=files_$r->{id}_content.innerHTML'>[$fc files]</a></div><div style='display: none' id='files_$r->{id}_content'><br>$files</div>" : '';
     my $ratio = (r10($r->{ratio}) or '--') . ($r->{maxratio} ? ' (' . r10($r->{maxratio}) . ')' : '');
     $ratio = "<div style='color: red'><b>$ratio</b></div>" if $r->{maxratio} and ($r->{ratio} > $r->{maxratio});
     $ratio = "<a onclick='set_maxratio_$r->{id}.innerHTML=set_maxratio_$r->{id}_content.innerHTML'>$ratio</a>" .
@@ -75,6 +72,8 @@ while (my $r = $sth->fetchrow_hashref) {
 	. "<input type=text name=maxratio value=$r->{maxratio} style='width: 50px'>" 
 	. submit({ -style => 'width: 30px', -value => 'OK' }) . endform);
     (my $up = $r->{up} ? ($r->{maxratio} ? '-' . fmsz(($r->{size} * $r->{maxratio} - $r->{up}) * (1 << 20)) : fmsz($r->{up} * (1 << 20))) : '--') =~ s/^--(.)/+$1/g;
+    my $files = join '<br>', map { '[' . fmsz($_->{size}) . '] ' . $_->{name} } @{$bt->{files}};
+    my $fc = scalar @{$bt->{files}};
     push @torrents, {
 	id => $r->{id},
 	active => $r->{active},
@@ -82,7 +81,8 @@ while (my $r = $sth->fetchrow_hashref) {
 	done => $r->{done},
 	icons => $statusimg,
 	name => $bt->{name},
-	files => $files,
+	files => ($fc > 1 ? $files : ''),
+	files_count => $fc,
 	size => $r->{size} ? fmsz($r->{size} * (1 << 20)) : '--',
 	up => $up,
 	down => fmsz($r->{down} * (1 << 20)),
