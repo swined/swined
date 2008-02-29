20886029adf844bd8f2743a319547bd920b83eb5
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-02-29 09:00:52 +0000

    auto commit

--- webtornado/pm/VER.pm
+++ webtornado/pm/VER.pm
@@ -1,1 +1,1 @@
-package VER; use Exporter; our @ISA = qw/Exporter/; our @EXPORT = qw/$VER/; our $VER = '0.0.120'; 1;
+package VER; use Exporter; our @ISA = qw/Exporter/; our @EXPORT = qw/$VER/; our $VER = '0.0.122'; 1;
