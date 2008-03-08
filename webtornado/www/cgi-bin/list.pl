b193c5c3ff4538da51e0721d640741811fb10058
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-03-08 21:28:25 +0000

    auto commit

--- webtornado/www/cgi-bin/list.pl
+++ webtornado/www/cgi-bin/list.pl
@@ -12,8 +12,6 @@ use Time::Duration;
 
 my $wt = new WT;
 
-sub IMG { $_[1] ? a({ -href => $_[1] }, img({ -src => $_[0] })) : img({ -src => $_[0] }) }
-
 sub r10 { int(10 * (shift or $_)) / 10 }
 
 sub fmsz {
@@ -49,9 +47,6 @@ while (my $r = $sth->fetchrow_hashref) {
 	$t->{progress} += int($r->{progress} * $r->{size} / 100);
 	$r->{active} and $t->{$_} += $r->{$_} for 'downrate', 'uprate', 'peers';
 	$t->{has_undone} = 1 unless $r->{done};
-	my $statusimg = IMG('/img/black.gif', "/start/$r->{id}");
-	$statusimg = IMG('/img/green.gif', "/stop/$r->{id}") if $r->{active} and $r->{pid};
-	$statusimg = IMG('/img/yellow.gif') if $r->{active} and ! $r->{pid} or ! $r->{active} and $r->{pid};
 	(my $up = $r->{maxratio} ? '-' . fmsz($r->{down} * $r->{maxratio} - $r->{up}) : fmsz($r->{up})) =~ s/^--(.)/+$1/g;
 	my $fc = scalar @{$bt->{files}};
 	$r->{seedstatus} = progressbar(100 * $r->{ratio} / $r->{maxratio}, $r->{uprate} ? ($r->{down} * $r->{maxratio} - $r->{up}) / $r->{uprate} : 0) if $r->{progress} == 100 and $r->{ratio} < $r->{maxratio};
@@ -60,7 +55,6 @@ while (my $r = $sth->fetchrow_hashref) {
 	push @torrents, { 
 		%$r,
 		user => $ENV{REMOTE_USER},
-		icons => $statusimg,
 		name => $bt->{name},
 		ue_name => uri_escape($bt->{name}),
 		overseed => ($r->{maxratio} and ($r->{ratio} > $r->{maxratio})),
