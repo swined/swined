7a3af40119eb967e662633d99da63a4a12b9ccca
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-03-10 13:15:16 +0000

    auto commit

--- webtornado/www/cgi-bin/list.pl
+++ webtornado/www/cgi-bin/list.pl
@@ -18,7 +18,7 @@ sub r10 { int(10 * (shift or $_)) / 10 }
 
 sub fmsz {
 	my $s = shift;
-	return r10 . 'T' if 1 < abs(local $_ = $s / (1 << 40));
+# 	return r10 . 'T' if 1 < abs(local $_ = $s / (1 << 40));
 	return r10 . 'G' if 1 < abs(local $_ = $s / (1 << 30));
 	return r10 . 'M' if 1 < abs(local $_ = $s / (1 << 20));
 	return r10 . 'k' if 1 < abs(local $_ = $s / (1 << 10));
