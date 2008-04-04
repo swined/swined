59d9080d8c5e6e10a806c04e106bd74087be0011
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-04-04 06:20:27 +0000

    auto commit

--- webtornado/www/script.js
+++ webtornado/www/script.js
@@ -8,3 +8,6 @@ function set_maxratio(id, maxratio) {
 		"</form>";
 	document.getElementById("set_maxratio_" + id).id = "set_maxratio_form_" + id;
 }
+function show_files(id) {
+    document.getElementById("files_" + id).innerHTML = document.getElementById("files_" + id + "_content").innerHTML;
+}
