9dd563708c743b9f2ecd845f4acc1822b70294ac
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-04-07 09:23:36 +0000

    auto commit

--- webtornado/www/script.js
+++ webtornado/www/script.js
@@ -1,6 +1,6 @@
 function set_maxratio(id, maxratio) {
 	document.getElementById("set_maxratio_" + id).innerHTML +=
-		"<br><form method=get action='/cgi-bin/action.pl'>" +
+		"<br><form method=get action='/webtornado/cgi-bin/action.pl'>" +
 		"<input type=hidden name=action value=set_maxratio>" +
 		"<input type=hidden name=id value=" + id + ">" +
 		"<input type=text name=maxratio value='" + maxratio + "' style='width: 50px'>" +
