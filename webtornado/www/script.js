d923176eeec763b76d7432a4de018e9b4656827d
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-04-10 06:04:17 +0000

    auto commit

--- webtornado/www/script.js
+++ webtornado/www/script.js
@@ -8,6 +8,33 @@ function set_maxratio(id, maxratio) {
 		"</form>";
 	document.getElementById("set_maxratio_" + id).id = "set_maxratio_form_" + id;
 }
+
 function show_files(id) {
-	document.getElementById("files_" + id).innerHTML = document.getElementById("files_" + id + "_content").innerHTML;
+	var xhr = get_xhr();
+	xhr.open('GET', '/webtornado?files=' + id, true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState != 4) { return }
+		if (xhr.status != 200) { return }
+		document.getElementById("files_" + id).innerHTML = xhr.responseText;
+	};
+	xhr.send(null);			 
+//	document.getElementById("files_" + id).innerHTML = document.getElementById("files_" + id + "_content").innerHTML;
 }
+
+function get_xhr(){
+	var xmlhttp;
+	try {
+    		xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+	} catch (e) {
+	        try {
+	        	xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		} catch (E) {
+			xmlhttp = false;
+		}
+	}
+	if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+		xmlhttp = new XMLHttpRequest();
+	}
+	return xmlhttp;
+}
+				      
