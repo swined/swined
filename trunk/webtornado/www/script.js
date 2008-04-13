63f23cccb94c57e0db6359990370680455474ad6
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-04-13 19:37:16 +0000

    auto commit

--- trunk/webtornado/www/script.js
+++ trunk/webtornado/www/script.js
@@ -1,12 +1,14 @@
 function set_maxratio(id, maxratio) {
-	document.getElementById("set_maxratio_" + id).innerHTML +=
+	div = document.getElementById("set_maxratio_" + id);
+	div.innerHTML +=
 		"<br><form method=get action='/webtornado/cgi-bin/action.pl'>" +
 		"<input type=hidden name=action value=set_maxratio>" +
 		"<input type=hidden name=id value=" + id + ">" +
 		"<input type=text name=maxratio value='" + maxratio + "' style='width: 50px'>" +
 		"<input type=submit style='width: 30px' value='OK'>" +
 		"</form>";
-	document.getElementById("set_maxratio_" + id).id = "set_maxratio_form_" + id;
+	div.onClick = function() {};
+	//document.getElementById("set_maxratio_" + id).id = "set_maxratio_form_" + id;
 }
 
 function show_files(id) {
@@ -19,8 +21,8 @@ function show_files(id) {
 		div.innerHTML = xhr.responseText;
 	};
 	div.innerHTML = '[loading]';
+	div.onClick = function() {};
 	xhr.send(null);			 
-//	document.getElementById("files_" + id).innerHTML = document.getElementById("files_" + id + "_content").innerHTML;
 }
 
 function get_xhr(){
@@ -39,4 +41,6 @@ function get_xhr(){
 	}
 	return xmlhttp;
 }
-				      
+
+function onLoad() {
+}
