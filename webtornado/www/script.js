9766eb967b6f8a761f50e25ba6e744ce684dc3b0
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-04-15 15:11:23 +0000

    auto commit

--- webtornado/www/script.js
+++ webtornado/www/script.js
@@ -28,10 +28,11 @@ function show_peers(id) {
 	var div = document.getElementById('peers_' + id);
 	var xhr = get_xhr();
 	xhr.open('GET', '/webtornado?peers=' + id, true);
+	var c = div.innerHTML;
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState != 4) { return }
 		if (xhr.status != 200) { return }
-		div.innerHTML = xhr.responseText;
+		div.innerHTML = c + ':' + xhr.responseText;
 	};
 	div.innerHTML = '<font color=gray>[loading]</font>';
 	div.setAttribute('onClick', '');
@@ -81,7 +82,7 @@ function onLoad() {
 		
 		var pc = row.cells.item(5);
 		pc.id = 'peers_' + id;
-		pc.setAttribute('onClick', 'show_peers(' + id + ')');
+		if (pc.innerHTML > 0) pc.setAttribute('onClick', 'show_peers(' + id + ')');
 
 		var rc = row.cells.item(6);
 		rc.id = 'set_maxratio_' + id;
