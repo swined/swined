f074a6de4bbcbdf887efaa108bc1253c0efedef0
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-08 08:37:15 +0000

    auto commit

--- trunk/swined-test-gae/youmbox.py
+++ trunk/swined-test-gae/youmbox.py
@@ -1,15 +1,21 @@
 from wsgiref.handlers import CGIHandler
 from google.appengine.ext.webapp import RequestHandler, WSGIApplication
+from google.appengine.api.urlfetch import fetch
 
 class UploadPage(RequestHandler):
+	def getFlvLink(self, url):
+		res = fetch(url)
+		if res.status_code == 200: return res.content
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-		self.response.out.write('youmbox/upload')
+		flvLink = self.getFlvLink('http://youtube.com/watch?v=q6mVvAZutV')
+		if not flvLink: return self.response.out.write('shit happened')
+		self.response.out.write(flvLink)
 
 class MainPage(RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-		self.response.out.write('youmbox')
+		self.response.out.write('<a href=upload>upload</a>')
 
 def main():
 	CGIHandler().run(WSGIApplication([
