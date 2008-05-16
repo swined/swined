90b4fd25e92ebc096bfbb40d0f389976c31d9c2c
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-16 12:11:03 +0000

    auto commit

--- trunk/+gae/swined-test/main.py
+++ trunk/+gae/swined-test/main.py
@@ -1,6 +1,11 @@
 from wsgiref.handlers import CGIHandler
 from google.appengine.ext.webapp import RequestHandler, WSGIApplication
 
+class TestPage(RequestHandler):
+	def get(self, a, b):
+		self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+		self.response.out.write('hellow: ' + a + ' / ' + b)
+
 class MainPage(RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
