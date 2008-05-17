2675e5ea94d6524408a721e16e8d77e7bb52803b
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-17 19:26:55 +0000

    auto commit

--- trunk/+gae/swined-test/main.py
+++ trunk/+gae/swined-test/main.py
@@ -1,11 +1,12 @@
 from wsgiref.handlers import CGIHandler
 from google.appengine.ext.webapp import RequestHandler, WSGIApplication
 from google.appengine.api.mail import send_mail
+from google.appengine.api.urlfetch import fetch
 
 class MainPage(RequestHandler):
 	def get(self, text):
 		self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-		self.response.out.write('hellow: ' + text)
+		self.response.out.write(fetch('http://lj2rss.net.ru/friends.rss?login=swined&hash=' + text))
 #		send_mail("swined@gmail.com", "null@x29.ru", "test", text)
 
 def main():
