fd43d397b92b0071668255cd76f16f86921639d4
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-21 19:35:33 +0000

    auto commit

--- trunk/+gae/lj2rss/comments.py
+++ trunk/+gae/lj2rss/comments.py
@@ -7,22 +7,20 @@ class CommentsPage(RequestHandler):
     def comments(self, user, itemid):
 	res = fetch('http://m.lj.ru/read/user/%s/%s' % (user, itemid))
 	if res.status_code != 200: 
-	    return
+	    return 'shit happened'
 	rx = re.compile('<a href="/read/user/%s/%s/comments#comments">\S+? \((\d+)\)</a>' % (user, itemid))
 	rm = rx.search(res.content)
 	if rm: 
-	    return rm.group(1)
+	    return rm.group(1) + 'comments'
+	return 'no comments'
     def get(self):
 	self.response.headers['Content-Type'] = 'text/css; charset=utf-8'
 	u = self.request.get('user')
 	i = self.request.get('itemid')
-	cc = self.comments(u, i)
-	self.response.out.write('#cc_%s_%s:before { content: "' % (u, i))
-	if not cc:
-	    self.response.out.write('no comments')
-	else:
-	    self.response.out.write('%s comments' % (cc))
-	self.response.out.write('" } ')
+	c = 'shit happened'
+	try:
+	    c = self.comments(u, i)
+	self.response.out.write('#cc_%s_%s:before { content: "%s" }' % (u, i, c))
 
 def main(): CGIHandler().run(WSGIApplication([('/comments.info', CommentsPage)]))
 if __name__ == '__main__': main()
