7b28944778e902850112339fc5af6a9c0e66b155
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-21 08:30:10 +0000

    auto commit

--- +gae/lj2rss/comments.py
+++ +gae/lj2rss/comments.py
@@ -1,7 +1,7 @@
 from wsgiref.handlers import CGIHandler
 from google.appengine.ext.webapp import RequestHandler, WSGIApplication
 
-class CommentsPage(Page):
+class CommentsPage(RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
     self.response.out.write('comments.info')
