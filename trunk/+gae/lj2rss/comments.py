aed882785bfb8b0dc1b3112fc40286a3ea4d8f04
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-21 08:28:55 +0000

    auto commit

---
+++ trunk/+gae/lj2rss/comments.py
@@ -0,0 +1,14 @@
+from wsgiref.handlers import CGIHandler
+from google.appengine.ext.webapp import RequestHandler, WSGIApplication
+
+class CommentsPage(Page):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+    self.response.out.write('comments.info')
+
+def main():
+  CGIHandler().run(WSGIApplication([
+    ('/comments.info', CommentsPage)
+  ]))
+
+if __name__ == '__main__': main()
