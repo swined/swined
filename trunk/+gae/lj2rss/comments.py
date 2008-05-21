6471f036509ec5319949dabddfa3b3bfc13284e5
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-21 14:14:57 +0000

    auto commit

--- trunk/+gae/lj2rss/comments.py
+++ trunk/+gae/lj2rss/comments.py
@@ -9,10 +9,16 @@ class CommentsPage(RequestHandler):
 	if res.status_code != 200: 
 	    return
 	rx = re.compile('<a href="/read/user/%s/%s/comments#comments">\S+? \((\d+)\)</a>' % (user, itemid))
-	return rx.search(res.content).group(1)
+	rm = rx.search(res.content)
+	if rm: 
+	    return rm.group(1)
     def get(self):
 	self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-	self.response.out.write(self.comments(self.request.get('user'), self.request.get('itemid')))
+	cc = self.comments(self.request.get('user'), self.request.get('itemid'))
+	if not cc:
+	    self.response.out.write('no comments')
+	else:
+	    self.response.out.write('%s comments' % (cc))
 
 def main(): CGIHandler().run(WSGIApplication([('/comments.info', CommentsPage)], debug = True))
 if __name__ == '__main__': main()
