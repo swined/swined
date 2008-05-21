d2ee5f1d811643d692d1418deb73891c62c41ae7
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-21 10:40:42 +0000

    auto commit

--- +gae/lj2rss/comments.py
+++ +gae/lj2rss/comments.py
@@ -4,17 +4,19 @@ from google.appengine.api import urlfetch
 
 def fetch(url, payload=None, method=urlfetch.GET, headers={}, allow_truncated=False):
     res = urlfetch.fetch(url, payload, method, headers, allow_truncated)
-    if res.status_code != 200: raise HttpError("http error " + str(res.status_code) + " (" + url + ")")
+    if res.status_code != 200: 
+	raise HttpError("http error " + str(res.status_code) + " (" + url + ")")
     return res
     
-class HttpError(Exception): pass
+class HttpError(Exception): 
+    pass
 
 class CommentsPage(RequestHandler):
     def ljsession(self, login, hash):
 	data = 'mode=sessiongenerate&expiration=short&user=' + login + '&hpassword=' + hash
-	res = fetch('http://www.livejournal.com/interface/flat', data, urlfetch.POST)
+	res = fetch('http://www.livejournal.com/interface/flat', data, urlfetch.POST).content
 	n = 0
-	for ljsession in res.content.split("\n"):
+	for ljsession in res.split("\n"):
 	    if n: return ljsession
 	    if ljsession == 'ljsession': n = 1
     def ljloggedin(self, ljsession):
@@ -24,7 +26,8 @@ class CommentsPage(RequestHandler):
 	self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
 	ljsession = self.ljsession(self.request.get('login'), self.request.get('hash'))
 	if not ljsession: return self.response.out.write('shit happened')
-	self.response.out.write(fetch(self.request.get('url')).content)
+	cookie = "ljloggedin=" + self.ljloggedin(ljsession) + "; ljsession=" + ljsession
+	self.response.out.write(fetch(self.request.get('url', headers = { 'Cookie' : cookie })).content)
 
 def main(): CGIHandler().run(WSGIApplication([('/comments.info', CommentsPage)], debug = True))
 if __name__ == '__main__': main()
