258d8f81a903bcda22d0171267a18a59191e91b3
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-21 10:13:40 +0000

    auto commit

--- trunk/+gae/lj2rss/comments.py
+++ trunk/+gae/lj2rss/comments.py
@@ -1,12 +1,18 @@
 from wsgiref.handlers import CGIHandler
 from google.appengine.ext.webapp import RequestHandler, WSGIApplication
-from google.appengine.api.urlfetch import fetch, GET, POST
+from google.appengine.api import urlfetch 
+
+def fetch(url, payload=None, method=urlfetch.GET, headers={}, allow_truncated=False):
+    res = urlfetch.fetch(url, payload, method, headers, allow_truncated)
+    if res.status_code != 200: raise HttpError("http error " + res.status_code)
+    return res
+    
+class HttpError(Exception): pass
 
 class CommentsPage(RequestHandler):
     def ljsession(self, login, hash):
 	data = 'mode=sessiongenerate&expiration=short&user=' + login + '&hpassword=' + hash
-	res = fetch('http://www.livejournal.com/interface/flat', data, POST)
-	if res.status_code != 200: return
+	res = fetch('http://www.livejournal.com/interface/flat', data, urlfetch.POST)
 	n = 0
 	for ljsession in res.content.split("\n"):
 	    if n: return ljsession
