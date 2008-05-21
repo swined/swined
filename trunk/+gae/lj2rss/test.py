63665d72e780f27d1e70288fea1ffa024383a68a
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-21 12:31:01 +0000

    auto commit

---
+++ trunk/+gae/lj2rss/test.py
@@ -0,0 +1,11 @@
+from urlfetch import fetch
+from wsgiref.handlers import CGIHandler
+from google.appengine.ext.webapp import RequestHandler, WSGIApplication
+
+class TestPage(RequestHandler):
+    def get(self):
+	self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+	self.response.out.write(fetch('http://ya.ru/'))
+
+def main(): CGIHandler().run(WSGIApplication([('/test.html', TestPage)], debug = True))
+if __name__ == '__main__': main()
