b80ba6569b413e35a9a60b049e61f78258bf0a5d
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-21 09:44:07 +0000

    auto commit

--- +gae/lj2rss/comments.py
+++ +gae/lj2rss/comments.py
@@ -1,14 +1,25 @@
 from wsgiref.handlers import CGIHandler
 from google.appengine.ext.webapp import RequestHandler, WSGIApplication
+from google.appengine.api.urlfetch import fetch, GET, POST
 
 class CommentsPage(RequestHandler):
-  def get(self):
-    self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-    self.response.out.write('comments.info')
+    def ljsession(self, login, hash):
+	data = 'mode=sessiongenerate&expiration=short&user=' + login + '&hpassword=' + hash
+	res = fetch('http://www.livejournal.com/interface/flat', data, POST)
+	if res.status_code != 200: return
+	n = 0
+	for ljsession in res.content.split("\n"):
+	    if n: return ljsession
+	    if ljsession == 'ljsession': n = 1
+    def get(self):
+	self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+	ljsession = self.ljsession(self.request.get('login'), self.request.get('hash'))
+	if not ljsession: return self.response.out.write('shit happened')
+	self.response.out.write(ljsession)
 
 def main():
-  CGIHandler().run(WSGIApplication([
-    ('/comments.info', CommentsPage)
-  ]))
+    CGIHandler().run(WSGIApplication([
+	('/comments.info', CommentsPage)
+    ]))
 
 if __name__ == '__main__': main()
