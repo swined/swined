4e36a3de938c94fd7f0f282204e5a7ed79b3c46f
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-23 07:30:40 +0000

    auto commit

--- trunk/+gae/lj2rss/comments.py
+++ trunk/+gae/lj2rss/comments.py
@@ -2,6 +2,23 @@ from wsgiref.handlers import CGIHandler
 from google.appengine.ext.webapp import RequestHandler, WSGIApplication
 from google.appengine.api.urlfetch import fetch
 import re
+import png
+
+class CommentsPngPage(RequestHandler):
+    def get(self):
+	self.response.headers['Content-Type'] = 'image/png'
+	width, height = 150, 20
+	c = PNGCanvas(width,height)
+	c.color = [0xff,0,0,0xff]
+	c.rectangle(0,0,width-1,height-1)
+	c.verticalGradient(1,1,width-2, height-2,[0xff,0,0,0xff],[0x20,0,0xff,0x80])
+        c.color = [0,0,0,0xff]
+	c.line(0,0,width-1,height-1)
+	c.line(0,0,width/2,height-1)
+	c.line(0,0,width-1,height/2)
+	c.copyRect(1,1,width/2-1,height/2-1,0,height/2,c)
+	c.blendRect(1,1,width/2-1,height/2-1,width/2,0,c)
+	self.response.out.write(c.dump())
 
 class CommentsSvgPage(RequestHandler):
     def comments(self, user, itemid):
@@ -24,5 +41,9 @@ class CommentsSvgPage(RequestHandler):
 <text x="3" y="16" font-family="Verdana" font-size="16" fill="blue">%s</text>
 </svg>""" % (c))
 
-def main(): CGIHandler().run(WSGIApplication([('/comments.svg', CommentsSvgPage)]))
+def main(): 
+    CGIHandler().run(WSGIApplication([
+	('/comments.svg', CommentsSvgPage),
+	('/comments.png', CommentsPngPage),
+    ]))
 if __name__ == '__main__': main()
