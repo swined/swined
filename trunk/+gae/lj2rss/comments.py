e425087eaedbadbb29c177e4adaff29c09281223
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-23 08:34:50 +0000

    auto commit

--- trunk/+gae/lj2rss/comments.py
+++ trunk/+gae/lj2rss/comments.py
@@ -15,7 +15,7 @@ class TextImage:
 	f = self.fc()
 #	for i in range(1, len(text)):
 #	    x = self.sc(text[i])
-	f.copyRect(5, 5, 10, 10, 0, 0, c)
+	f.copyRect(5, 0, 10, 10, 0, 0, c)
 #	    f.copyRect(x, 1, x + self.fw, self.fh, self.fw * (i - 1) + 1, 1, c)
 	self.rc = c
     def fc(self):
