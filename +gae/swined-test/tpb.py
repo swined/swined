d8c335568ce1b9c99eeb6b801f09e3ee1ca5340d
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-28 07:14:54 +0000

    auto commit

--- +gae/swined-test/tpb.py
+++ +gae/swined-test/tpb.py
@@ -3,12 +3,12 @@ from google.appengine.ext.webapp import RequestHandler, WSGIApplication
 from google.appengine.api.urlfetch import fetch
 
 class RssPage(RequestHandler):
-	def get(self, qw):
+	def get(self, ct, qw):
 		self.response.headers['Content-Type'] = 'text/xml; charset=utf-8'
-		res = fetch('http://thepiratebay.org/rss')
+		res = fetch('http://rss.thepiratebay.org/' + ct)
 		self.response.out.write(res.content)
 
 def main():
-	CGIHandler().run(WSGIApplication([('/tpb/(.*)', RssPage)], debug = True))
+	CGIHandler().run(WSGIApplication([('/tpb/(.*)/(.*)', RssPage)], debug = True))
 
 if __name__ == '__main__': main()
