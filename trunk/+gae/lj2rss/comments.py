ffbb570912070cfa5f66ae3fd960e15e8faf860b
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-28 09:40:50 +0000

    auto commit

--- trunk/+gae/lj2rss/comments.py
+++ trunk/+gae/lj2rss/comments.py
@@ -13,6 +13,9 @@ class TextImage:
     fs = '0123456789 abcdefghijklmnopqrstuvwxyz'
     rc = None
     def __init__(self, text):
+	global font
+	if font:
+	    text = text + ' x'
 	c = PNGCanvas(self.fw * (len(text) + 1), self.fh + 5)
 	f = self.fc()
 	for i in range(0, len(text)):
