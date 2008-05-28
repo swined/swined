9a03676d8f99d399c035813d15c03c9c432553af
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-28 12:49:24 +0000

    auto commit

--- trunk/+gae/lj2rss/comments.py
+++ trunk/+gae/lj2rss/comments.py
@@ -96,7 +96,7 @@ class StatsPage(RequestHandler):
 	for i in range(2, 23):
 	    xl = xl + '|-' + str(23 - i)
 	xl = xl + '|now'
-	c = 'cht=lc&chs=320x120&chg=10,50&chxl=' + xl + '&chd=t:' + ','.join(s)
+	c = 'cht=lc&chs=320x120&chg=10,25&chxt=x&chxl=' + xl + '&chd=t:' + ','.join(s)
 	self.response.out.write('<img src="http://chart.apis.google.com/chart?' + c + '">')
 
 app = WSGIApplication([('/comments.png', CommentsPngPage), ('/stats.html', StatsPage)])
