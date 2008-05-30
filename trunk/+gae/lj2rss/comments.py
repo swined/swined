4f4ab7ace68f84282a8d76f22b3631975f213ef6
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-30 10:13:24 +0000

    auto commit

--- trunk/+gae/lj2rss/comments.py
+++ trunk/+gae/lj2rss/comments.py
@@ -2,15 +2,8 @@ from wsgiref.handlers import CGIHandler
 from google.appengine.ext.webapp import RequestHandler, WSGIApplication
 from google.appengine.ext import db
 from google.appengine.api.urlfetch import fetch
-from google.appengine.api import memcache
 from png import PNGCanvas
-from time import time
-import datetime
 import re
-
-class Request(db.Model):
-    time = db.DateTimeProperty(auto_now_add = True)
-    service = db.StringProperty()
 
 class Font:
     width = 0
@@ -68,56 +61,10 @@ class CommentsPngPage(RequestHandler):
 	else: 	
 	    return 'no comments'
     def get(self):
-	k = 'requests_' + str(int(time() / 60))
-	if not memcache.incr(k):
-	    memcache.set(k, '1', 60 * 60 * 48)
-        Request(service = 'comments.png').put()
 	self.response.headers['Content-Type'] = 'image/png'
 	TextImage(self.comments(self.request.get('user'), self.request.get('itemid'))).dump(self.response.out)
 
-class StatsPage(RequestHandler):
-    def stats(self):
-	r = []
-    	for i in range(1, 150):
-	    q = db.Query(Request)
-	    q.filter('time > ', datetime.datetime.now() - datetime.timedelta(minutes = i * 10))
-	    q.filter('time < ', datetime.datetime.now() - datetime.timedelta(minutes = (i - 1) * 10))
-	    r.append(str(q.count() / 10))
-	r.reverse()
-	return r
-    def stats1(self):
-	r = []
-	t = int(time() / 60)
-    	for i in range(0, 60):
-    	    k = 'requests_' + str(t - i)
-    	    v = memcache.get(k)
-    	    if not v:
-    		v = 0
-	    r.append(str(v))
-	r.reverse()
-	return r	
-    def max(self, a):
-	m = 0
-	for e in a:
-	    if int(e) > m:
-		m = int(e)
-	return m
-    def get(self):
-	self.response.headers['Content-Type'] = 'text/html'
-	s = self.stats()
-	S = []
-	m = self.max(s) * 1.1
-	for i in s:
-	    S.append(str(int(100 * int(i) / m)))
-	c = 'cht=lc&chs=800x160&chg=4,25&chxt=y&chxr=0,0,' + str(int(m)) + '&chd=t:' + ','.join(S)
-	self.response.out.write('<meta http-equiv="REFRESH" content="30;URL=/stats.html"></meta>')
-	self.response.out.write('<img src="http://chart.apis.google.com/chart?' + c + '"><br>')
-#	s = self.stats1()
-#	m = self.max(s)
-#	c = 'cht=lc&chs=480x160&chg=10,25&chxt=x,y&chxt=x,y&chxr=0,0,24|1,0,' + str(m) + '&chd=t:' + ','.join(s)
-#	self.response.out.write('<img src="http://chart.apis.google.com/chart?' + c + '"><br>')
-
-app = WSGIApplication([('/comments.png', CommentsPngPage), ('/stats.html', StatsPage)], debug = True)
+app = WSGIApplication([('/comments.png', CommentsPngPage)], debug = True)
 
 def main(): 
     global app
