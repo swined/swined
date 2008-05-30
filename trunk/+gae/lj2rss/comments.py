7ac46ec42c124d45f3a392109bf32fc36a073713
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-30 09:56:24 +0000

    auto commit

--- trunk/+gae/lj2rss/comments.py
+++ trunk/+gae/lj2rss/comments.py
@@ -106,10 +106,10 @@ class StatsPage(RequestHandler):
 	self.response.headers['Content-Type'] = 'text/html'
 	s = self.stats()
 	S = []
-	m = int(self.max(s) * 1.1)
+	m = self.max(s) * 1.1
 	for i in s:
 	    S.append(str(int(100 * int(i) / m)))
-	c = 'cht=lc&chs=480x160&chg=4,25&chxt=y&chxr=0,0,' + str(m) + '&chd=t:' + ','.join(S)
+	c = 'cht=lc&chs=480x160&chg=4,25&chxt=y&chxr=0,0,' + str(int(m)) + '&chd=t:' + ','.join(S)
 	self.response.out.write('<meta http-equiv="REFRESH" content="30;URL=/stats.html"></meta>')
 	self.response.out.write('<img src="http://chart.apis.google.com/chart?' + c + '"><br>')
 #	s = self.stats1()
