fb24d6a9eeb0e7d54e54e24161966ea08039a2ab
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-30 10:55:47 +0000

    auto commit

--- trunk/+gae/swined-test/img.py
+++ trunk/+gae/swined-test/img.py
@@ -1,17 +1,42 @@
 from wsgiref.handlers import CGIHandler
-from google.appengine.ext.webapp import RequestHandler, WSGIApplication
-from google.appengine.api import memcache
+from google.appengine.ext import webapp
+from google.appengine.ext import db
+from google.appengine.api import users
+from google.appengine.api import images
 
-class MainPage(RequestHandler):
+class Image(db.Model):
+	owner = db.UserProperty()
+	image = db.BlobProperty()
+	comment = db.StringProperty(multiline = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+	accessed = db.DateTimeProperty(auto_now = True)
+	requests = db.IntegerProperty()
+
+class UserPage(webapp.RequestHandler):
 	def get(self):
-		if not memcache.incr('tv'):
-		    memcache.set('tv', '1')
+		self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+		self.response.out.write('images')
+		q = Image.all()
+		q.filter('user = ', users.get_current_user())
+		for i in q.fetch():
+		    self.response.out.write('<img src="http://x29.ru/i/p/%s"><br>' % (i.id))
+	def post(self):
+		i = Image()
+		i.owner = users.get_current_user()
+		i.image = self.request.get('image')
+		i.put()
+		self.redirect('/i/u')
+
+class MainPage(webapp.RequestHandler):
+	def get(self):
 		self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
 		self.response.out.write('hellow (%s)' % (memcache.get('tv')))
 
 def main():
-	CGIHandler().run(WSGIApplication([
-		('/', MainPage),
+	CGIHandler().run(webapp.WSGIApplication([
+		('/i', MainPage),
+		('/i/u', UserPage),
 	], debug = True))
 
-if __name__ == '__main__': main()
+if __name__ == '__main__': 
+	main()
