d27f63ad14cc5caa0233acc81a887d7a07614e37
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-05-30 10:04:27 +0000

    auto commit

--- trunk/+gae/swined-test/main.py
+++ trunk/+gae/swined-test/main.py
@@ -1,10 +1,13 @@
 from wsgiref.handlers import CGIHandler
 from google.appengine.ext.webapp import RequestHandler, WSGIApplication
+from google.appengine.api import memcache
 
 class MainPage(RequestHandler):
 	def get(self):
+		if not memcache.incr('tv'):
+		    memcache.set('tv', 1)
 		self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-		self.response.out.write('hellow')
+		self.response.out.write('hellow ()' % (memcache.get('tv')))
 
 def main():
 	CGIHandler().run(WSGIApplication([
