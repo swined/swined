7dbad5655f152fb94cd07480f315c078ca0776a8
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-06-16 12:33:11 +0000

    auto commit

--- trunk/webtornado/www/cgi-bin/list.pl
+++ trunk/webtornado/www/cgi-bin/list.pl
@@ -106,7 +106,7 @@ foreach my $r (sort { $b->{ratio} <=> $a->{ratio} } map { $q->{$_} } keys %$q) {
 	$r->{seedstatus} = progressbar(100 * $r->{ratio} / $r->{maxratio}, $r->{uprate} ? ($r->{down} * $r->{maxratio} - $r->{up}) / $r->{uprate} : 0) if $r->{progress} == 100 and $r->{ratio} < $r->{maxratio};
 	$r->{$_} = fmsz($r->{$_}) for 'size', 'down', 'uprate', 'downrate';
 	$r->{$_} = r10($r->{$_}) for 'ratio', 'maxratio';
-	$bt->{announce} =~ s|^https?://([^/]+)/.*$|$1|i;
+	$bt->{announce} =~ s|^https?://([^/]+)(?::\d+)?/.*$|$1|i;
 	push @torrents, {
 		%$r, %hc,
 		user => $ENV{REMOTE_USER},
