64f779a32634097808f650d779538c714598e184
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-08-25 06:43:02 +0000

    auto commit

--- trunk/+gae/swined-test/lj2rss.py
+++ trunk/+gae/swined-test/lj2rss.py
@@ -38,23 +38,27 @@ class LJ:
 		ses = self.getSession()
 		t = ses.split(':')
 		return t[1] + ':' + t[2]
+	def getCookies(self):
+		return 'ljsession=' + self.getSession() + '; ljloggedin=' + self.getLoggedIn() + '; '
 	def getList(self, skip = 0):
 		res = self.fetch(
 			'http://www.livejournal.com/mobile/friends.bml?skip=' + str(skip),
-			headers = { 'Cookie' : 'ljsession=' + self.getSession() + '; ljloggedin=' + self.getLoggedIn() }
+			headers = { 'Cookie' : self.getCookies() }
 		).content
 		rx = re.compile(": <a href='(http://.*?/\d+\.html)\?format=light'>")
 		rr = []
 		for m in rx.finditer(res):
 			rr.append(m.group(1))
 		return rr
+	def getEntry(self, url):
+		return url
 
 class MainPage(webapp.RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
 		lj = LJ(self.request.get('login'), self.request.get('hash'))
 		for url in lj.getList():
-			self.response.out.write(url + '<br>')
+			self.response.out.write(self.getEntry(url) + '<br>')
 
 def main():
 	handlers.CGIHandler().run(webapp.WSGIApplication([
