aff173e0eb0bf6d03032de85f75e36d3666c3885
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-08-26 12:01:22 +0000

    auto commit

--- webtornado/www/cgi-bin/get.pl
+++ webtornado/www/cgi-bin/get.pl
@@ -9,7 +9,7 @@ my $wt = new WT;
 my $tor = $wt->selectrow_hashref('SELECT * FROM torrents WHERE id = ?', undef, $wt->cgi->param('id'));
 my $bt = WT::getTorrentInfo(uri_unescape $tor->{torrent});
 my $ts = 0;
-$ts += 512 + $_->{size} + ($_->{size} % 512) ? (512 - $_->{size} % 512) : 0 for @{$bt->{files}};
+$ts += 512 + $_->{size} + (($_->{size} % 512) ? (512 - $_->{size} % 512) : 0) for @{$bt->{files}};
 
 print($wt->cgi->header(-status => 404)) and exit unless $tor->{output} and -e $tor->{output};
 print $wt->cgi->header(
