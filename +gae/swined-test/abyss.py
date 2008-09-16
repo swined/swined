811799e4798157ca000f1789b18d08593391ede9
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-09-16 07:23:00 +0000

    auto commit

--- +gae/swined-test/abyss.py
+++ +gae/swined-test/abyss.py
@@ -1,6 +1,6 @@
-from google.appengine.ext.webapp.util import *
-from google.appengine.ext.webapp import run_wsgi_app
-from google.appengine.api.urlfetch import fetch
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import WSGIApplication, RequestHandler
+from google.appengine.api.ulfetch import fetch
 import re
 
 class MainPage(RequestHandler):
