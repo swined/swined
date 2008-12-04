a0631a534d29d1e2dcae1a4b55f13b318aed919f
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-12-04 12:33:42 +0000

    auto commit

--- trunk/+gae/swined-test/test.py
+++ trunk/+gae/swined-test/test.py
@@ -1,1 +1,16 @@
 from swf import UserAgent
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import WSGIApplication, RequestHandler
+
+class MainPage(RequestHandler):
+	def get(self):
+		self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+		self.response.out.write('hellow')
+
+def main():
+	run_wsgi_app(WSGIApplication([
+		('/test', MainPage),
+	], debug = True))
+
+if __name__ == '__main__': 
+	main()
