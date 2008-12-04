d473b4e786ed228726f6d8230b83886745909027
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-12-04 13:39:07 +0000

    auto commit

--- +gae/swined-test/test.py
+++ +gae/swined-test/test.py
@@ -5,7 +5,11 @@ from google.appengine.ext.webapp import WSGIApplication, RequestHandler
 class MainPage(RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-		self.response.out.write('hellow')
+		ua = UserAgent()
+		ua.get('http://ya.ru/')
+		for k,v in ua.cookies:
+			self.response.out.write('%s=%s<br>' % (k,v))
+		self.response.out.write(ua.response.content)
 
 def main():
 	run_wsgi_app(WSGIApplication([
