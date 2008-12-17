e46ec019906f7e8e5119edfe17cd00db7533f2c2
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2008-12-17 11:14:22 +0000

    auto commit

--- +gae/swined-test/test.py
+++ +gae/swined-test/test.py
@@ -11,9 +11,18 @@ class MainPage(RequestHandler):
 #			self.response.out.write('%s=%s<br>' % (k,v))
 #		self.response.out.write(ua.response.content)
 
+class FetchPage(RequestHandler):
+	def get(self):
+		self.response.headers['Content-Type'] = 'text/xml; charset=utf-8'
+		ua = UserAgent()
+		ua.get('http://lj2mail.net.ru/friends.rss?login=%s&hash=%s' % (self.request.get('login'), self.request.get('hash')))
+		self.response.out.write(ua.response.content)
+
+
 def main():
 	run_wsgi_app(WSGIApplication([
 		('/test', MainPage),
+		('/fetch', MainPage),
 	], debug = True))
 
 if __name__ == '__main__': 
