36fbe6375361a74dac3b1625586acbd202045f6c
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2009-03-02 08:50:30 +0000

    auto commit

--- +gae/swined-test/nb.py
+++ +gae/swined-test/nb.py
@@ -1,3 +1,4 @@
+from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import WSGIApplication, RequestHandler
