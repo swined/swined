a79f6dbcfcb16e563d63eb1835ca7846228d1575
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2009-03-02 08:46:28 +0000

    auto commit

--- trunk/+gae/swined-test/nb.py
+++ trunk/+gae/swined-test/nb.py
@@ -1,11 +1,43 @@
+from google.appengine.api import users
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import WSGIApplication, RequestHandler
+
+class Note(db.Model):
+	owner = db.ListProperty(db.UserProperty)
+	mtime = db.DateTimeProperty(auto_now = true)
+	tags = db.StringListProperty()
+	text = db.TextProperty()
+
+class Notebook():
+	user = None
+	def __init__(self, user):
+		self.user = user
+	def tags(self):
+		#
+	def list(self, tags):
+		#
+	def add(self, text, tags):
+		Note(
+			owner = db.List(db.User(self.user)),
+			text = db.Text(text), 
+			tags = db.StringList(tags),
+		).put();
+	def del(self, id):
+		Note()
+	def edit(self, id, text, tags):
+		#
+	def share(self, id, user):
+		#
+	def unshare(self, id, user):
+		#
 
 class MainPage(RequestHandler):
         def get(self):
                 self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+		nb = Notebook(users.get_current_user())
+		nb.add('test', ['1', '2', '3'])
 		self.response.out.write('test')
-
+		
 def main():
         run_wsgi_app(WSGIApplication([
                 ('/nb', MainPage),
