7196919e359f3f7ab7456476c24b17d47361dd58
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2009-03-02 07:00:54 +0000

    auto commit

---
+++ +gae/swined-test/nb.py
@@ -0,0 +1,17 @@
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import WSGIApplication, RequestHandler
+
+class MainPage(RequestHandler):
+        def get(self):
+                self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+                ua = UserAgent()
+		self.response.out.write('test')
+
+def main():
+        run_wsgi_app(WSGIApplication([
+                ('/nb', MainPage),
+        ], debug = True))
+
+if __name__ == '__main__':
+        main()
+
