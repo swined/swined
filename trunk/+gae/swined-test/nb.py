9bfe328881fdbe35f54717be73c4edc54b2593a8
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2009-03-03 07:11:14 +0000

    auto commit

--- trunk/+gae/swined-test/nb.py
+++ trunk/+gae/swined-test/nb.py
@@ -69,7 +69,7 @@ class MainPage(RequestHandler):
         def get(self):
 		nb = Notebook(users.get_current_user())
                 self.response.headers['Content-Type'] = 'text/xml; charset=utf-8'
-		self.response.out.write('<?xml version="1.0" encoding="UTF-8"?>') .
+		self.response.out.write('<?xml version="1.0" encoding="UTF-8"?>')
 		self.response.out.write('<?xml-stylesheet href="/notes.xsl" type="text/xsl"?>')
 		self.response.out.write('<nb>')
 		for note in nb.list([]):
