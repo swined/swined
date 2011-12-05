c93f9cadbe372484bdc39e191e2a282d0b632dc4
swined <swined@392ebfb8-ef44-0410-ba30-a97371fef0d7>
2011-12-05 20:04:33 +0000

    m

--- battery/Main.java
+++ battery/Main.java
@@ -2,19 +2,22 @@ import java.awt.Toolkit;
 import java.awt.Image;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
+import java.awt.Dimension;
 
 public class Main {
 
 	public static void main(String[] args) throws Throwable {
 		SystemTray tray = SystemTray.getSystemTray();
-		System.out.println(tray.getTrayIconSize());
-		Image icon = Toolkit.getDefaultToolkit().createImage("icons/battery_charge/battery-good-charging.png");
-		TrayIcon trayIcon = new TrayIcon(icon, "battery", null);
+		Dimension size = tray.getTrayIconSize();
+		TrayIcon trayIcon = new TrayIcon(ImageUtils.textIcon(size, "?"), "battery charge", null);
 		tray.add(trayIcon);
 		while (true) {
+			float charge = BatteryStatus.getChargePercentage();
 			System.out.println(
-				BatteryStatus.getChargePercentage() + "% " + 
+				charge + "% " + 
 				BatteryStatus.getTimeToLive() + "h");
+			trayIcon.setImage(ImageUtils.textIcon(size, Float.toString(charge)));
+			trayIcon.setToolTip(Float.toString(charge) + "%");
 			Thread.sleep(5000);
 		}
 	}
